37d0077ff7b1aa2dcd16e59f6521a20118341473
Shugi9 <105164355+Shugi9@users.noreply.github.com>
2022-05-09 14:55:31 -0500

    DogYears
    description of hw

---
+++ dog years.js
@@ -0,0 +1,12 @@
+// variable set for my age
+var myAge=30;
+// early years set to two, this variable will change
+var earlyYears=(2 * 10.5);
+var laterYears=((myAge-2)*4);
+// console.log(laterYears * 4);
+myAgeInDogYears=(earlyYears + laterYears);
+var inputName
+var myName
+inputName = 'Ryan'
+
+console.log (`My name is ${inputName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
